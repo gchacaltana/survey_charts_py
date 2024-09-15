fdf3d787b656e30fa5486029436882bb9734bb4f
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-09-15 16:05:15 -0500

    add settings

---
+++ settings.py
@@ -0,0 +1,12 @@
+"""
+settings.py
+"""
+
+COLOR_YES = "#3366CC"
+COLOR_NO = "#DC3912"
+
+# Lista de valores que consideramos como "si"
+AFFIRMATIVE_VALUES = ["si", "sí", "yes", "y", "s", "true"]
+
+# Lista de valores que consideramos como "no"
+NEGATIVE_VALUES = ["no", "n", "not", "false"]
